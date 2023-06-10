665d6e844115a8e534f1c6615365c924660d4b9e
cilantrofe <131597495+cilantrofe@users.noreply.github.com>
2023-06-10 21:29:25 +0300

    Create stage_2.cpp

---
+++ test_tasking/stage_2/stage_2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+int findContainer(const std::vector<std::vector<std::string>>& containers, const std::string& target) {
+    std::unordered_map<std::string, const std::vector<std::string>*> containerMap;  // Хранит соответствие строка -> контейнер
+
+    // Заполняем хэш-таблицу для доступа к контейнеру по строке
+    for (const std::vector<std::string>& container : containers) {
+        for (const std::string& str : container) {
+            containerMap[str] = &container;
+        }
+    }
+
+    if (containerMap.find(target) != containerMap.end()) {
+        const std::vector<std::string>* container = containerMap[target];
+        int containerIndex = std::distance(&containers[0], container);
+        return containerIndex;
+    } else {
+        return -1;
+    }
+}
+
+int main() {
+    std::vector<std::vector<std::string>> containers = {
+            {"строка3", "строка1", "строка2"},
+            {"строка4", "строка5"},
+            {"строка8", "строка7", "строка6"},
+            {"строка9"}
+    };
+
+    std::cout << "Начальное наполнение контейнеров:\n";
+    for (int i = 0; i < containers.size(); i++) {
+        std::cout << "Контейнер " << i << ": ";
+        for (const std::string& str : containers[i]) {
+            std::cout << str << ' ';
+        }
+        std::cout << '\n';
+    }
+
+    std::string searchString;
+    std::cout << "\nВведите строку для поиска: ";
+    std::getline(std::cin, searchString);
+
+    int containerIndex = findContainer(containers, searchString);
+    if (containerIndex != -1) {
+        std::cout << "Строка найдена в контейнере " << containerIndex << '\n';
+    } else {
+        std::cout << "Строка не найдена в контейнерах" << '\n';
+    }
+
+    return 0;
+}
+
